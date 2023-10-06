cea13a5207ece9cd5153acf6e4e7a63833a1f047
Hamie <h.nouri@sae.edu>
2023-10-06 17:51:09 +0100

    Added Pause on P into GM

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private int CollectedCoin = 0;
+    private bool isPaused = false;
 
     // Call this method when a coin is collected.
     public void AddCollectedCoin(int amount)
@@ -18,5 +19,24 @@ public class GameManager : MonoBehaviour
         }
 
         // You can add any other logic here, such as updating a UI display of collected coins.
+    }
+    
+    void Update()
+    {
+        // Check for player input to toggle pause
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+            {
+                // Unpause the game
+                Time.timeScale = 1.0f;
+            }
+            else
+            {
+                // Pause the game
+                Time.timeScale = 0.0f;
+            }
+            isPaused = !isPaused;
+        }
     }
 }
